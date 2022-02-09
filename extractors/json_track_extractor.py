8b96649d78dac6be3b2b08a243a34442900683b1
Noabelle Robins <noabelle3@gmail.com>
2022-02-09 15:25:18 +0200

    added exception handling and logs to JsonTrackExtractor class

--- extractors/json_track_extractor.py
+++ extractors/json_track_extractor.py
@@ -1,5 +1,6 @@
 import json
 import os
+import logging
 from extractors.file_extractor import FileExtractor
 
 
@@ -7,9 +8,13 @@ class JsonTrackExtractor(FileExtractor):
     def __init__(self, src_directory):
         self.src_directory = src_directory
 
+
     def extract(self):
         extracted_jsons = []
-        for file in os.listdir(self.src_directory):
-            with open(file) as json_track:
-                extracted_jsons.append(json.load(json_track))
-        return extracted_jsons
+        try:
+            for file in os.listdir(self.src_directory):
+                with open(f'{self.src_directory}\\{file}') as json_track:
+                    extracted_jsons.append(json.load(json_track))
+            return extracted_jsons
+        except FileNotFoundError:
+            logging.error("Directory doesn't exist")

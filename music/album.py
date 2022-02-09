1278786882faf3632d8f5cc522bae222a6915f7b
Noabelle Robins <noabelle3@gmail.com>
2022-02-09 11:50:09 +0200

    Added artist_id to Album constructor

--- music/album.py
+++ music/album.py
@@ -1,6 +1,7 @@
 class Album:
-    def __init__(self, album_id, name, tracks):
+    def __init__(self, album_id, name, artist_id, tracks):
         self.album_id = album_id
         self.name = name
+        self.artist_id = artist_id
         self.tracks = tracks
 

4b9157c92fa6c34c30664ab43a18fcc7c5c172f8
Noabelle Robins <noabelle3@gmail.com>
2022-02-09 14:22:27 +0200

    Added option to add songs to an album or add albums to an artist

--- music/album.py
+++ music/album.py
@@ -1,7 +1,15 @@
+from music.exceptions.music_exceptions import TrackAlreadyExistsException
+
+
 class Album:
-    def __init__(self, album_id, name, artist_id, tracks):
+    def __init__(self, album_id, name, artist_id):
         self.album_id = album_id
         self.name = name
         self.artist_id = artist_id
-        self.tracks = tracks
+        self.tracks = {}
 
+    def add_track_to_album(self, track):
+        if track.track_id in self.tracks:
+            raise TrackAlreadyExistsException
+        else:
+            self.tracks[track.track_id] = track
--- music/artist.py
+++ music/artist.py
@@ -1,5 +1,14 @@
+from music.exceptions.music_exceptions import AlbumAlreadyExistsException
+
+
 class Artist:
     def __init__(self, artist_id, name, albums):
         self.artist_id = artist_id
         self.name = name
-        self.albums = albums
+        self.albums = {}
+
+    def add_album_to_artist(self, album):
+        if album.album_id in self.albums:
+            raise AlbumAlreadyExistsException
+        else:
+            self.albums[album.album_id] = album

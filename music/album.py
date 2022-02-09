1c0e99ab0599d6ac8a97c1d2177fca8a6ad9c003
Noabelle Robins <noabelle3@gmail.com>
2022-02-09 09:57:44 +0200

    Added Album class, changed contructor in Track class

---
+++ music/album.py
@@ -0,0 +1,6 @@
+class Album:
+    def __init__(self, name, _id, tracks):
+        self.id = _id
+        self.name = name
+        self.tracks = tracks
+
--- music/track.py
+++ music/track.py
@@ -1,7 +1,7 @@
 class Track:
-    def __init__(self, track):
-        self.album = track['album']
-        self.artists = track['artists']
-        self.id = track['id']
-        self.name = track['name']
-        self.popularity = track['popularity']
+    def __init__(self, album, artists, _id, name, popularity):
+        self.album = album
+        self.artists = artists
+        self.id = _id
+        self.name = name
+        self.popularity = popularity

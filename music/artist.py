65cac1f1f3823fc430c2092198fd83290990fbe6
Noabelle Robins <noabelle3@gmail.com>
2022-02-10 11:03:05 +0200

    Made Artist class inherit User class

--- music/artist.py
+++ music/artist.py
@@ -1,8 +1,11 @@
+import logging
 from music.exceptions.music_exceptions import AlbumAlreadyExistsException
+from user.user import User
 
 
-class Artist:
+class Artist (User):
     def __init__(self, artist_id, name, albums):
+        super().__init__(name)
         self.artist_id = artist_id
         self.name = name
         self.albums = {}
@@ -12,3 +15,15 @@ class Artist:
             raise AlbumAlreadyExistsException
         else:
             self.albums[album.album_id] = album
+
+        def add_new_playlist(self, playlist_name):
+            super().add_new_playlist(playlist_name)
+
+    def add_track_to_playlist(self, playlist_name, track_id, track_index):
+        try:
+            self.playlists[playlist_name].add_track(track_index.tracks[track_id])
+        except KeyError:
+            if playlist_name not in self.playlists:
+                logging.error("Playlist does not exist")
+            else:
+                logging.error("Track ID does not exist")
